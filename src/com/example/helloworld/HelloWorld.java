a1cc6a7a96ee5b5977ea7569da98b5968e38f1a2
Joshaline <joshuachinwendu@gmail.com>
2021-05-25 17:55:13 +0100

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Variables
+        int a, b, c;    // Declaring a, b and c
+        int d = 4, e, f = 6;    // Declaring d, e, and f BUT initializing d and f
+        double pi = 3.14555;
+        byte z = 22;
+        char x = 'x';
+
+        double y = Math.sqrt(d+f);  // Dynamic declaration
+
+
+        //  Arrays
+        int month_days [] = new int [12];
+
+        System.out.println("Hello, World!");
+        System.out.println(y);
+    }
+}
