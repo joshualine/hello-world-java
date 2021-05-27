d3ae8370bc9ea512de4355674677a4d140c984c5
Joshaline <joshuachinwendu@gmail.com>
2021-05-27 16:52:46 +0100

    second commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,21 +1,72 @@
 package com.example.helloworld;
 
+import java.util.Arrays;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        // Variables
-        int a, b, c;    // Declaring a, b and c
-        int d = 4, e, f = 6;    // Declaring d, e, and f BUT initializing d and f
-        double pi = 3.14555;
-        byte z = 22;
-        char x = 'x';
+//        // Variables
+//        int a, b, c;    // Declaring a, b and c
+//        int d = 4, e, f = 6;    // Declaring d, e, and f BUT initializing d and f
+//        double pi = 3.14555;
+//        byte z = 22;
+//        char x = 'x';
+//
+//        double y = Math.sqrt(d);  // Dynamic declaration
+//
+//        //  Arrays can be initialized when they are declared.
+//        int[] month_days = {31, 28, 31, 30, 30, 31};
+//
+//        double[] numbers = {10.2, 34.6, 67.6, 86.4, 45.6};
+//        double result = 0;
+//        int i;
+//        for (i=0; i<5; i++)
+//            result = result + numbers[i];
+//        System.out.println("The average is " + result/5);
 
-        double y = Math.sqrt(d+f);  // Dynamic declaration
+
+//        System.out.println("Hello, World!");
+//        System.out.println(y);
+//        System.out.println(month_days[0]);
+
+        // Multidimensional arrays
+//        int[][] twoD = new int[4][5];
+//        int i, j, k = 0;
+//
+//        for(i=0; i<4; i++)
+//            for(j=0; j<5; j++) {
+//                twoD[i][j] = k;
+//                k++;
+//            }
+//
+//        for(i=0; i<4; i++) {
+//            for(j=0; j<5; j++)
+//                System.out.print(twoD[i][j] + " ");
+//            System.out.println();
+//        }
+
+//        double[][] m = {
+//                { 2, 3, 5, 5 },
+//                { 4, 3, 5, 6 },
+//                { 2, 4, 5, 6 },
+//                { 7, 3, 4, 5 }
+//        };
+//        int i, j;
+//        for(i=0; i<4; i++) {
+//            for(j=0; j<4; j++)
+//                System.out.print(m[i][j]);
+//            System.out.println();
+//        }
+
+        // 3D arrays---------------
+        // code in page 133
 
 
-        //  Arrays
-        int month_days [] = new int [12];
 
-        System.out.println("Hello, World!");
-        System.out.println(y);
+
+
+
+
+
+
     }
 }
